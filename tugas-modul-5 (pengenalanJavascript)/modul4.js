4dc05ecfe0a4102680e02b7e9744142b65fb2f32
FadhilMuh1607 <108079861+FadhilMuh1607@users.noreply.github.com>
2022-06-23 22:04:22 +0700

    Modul 5 - (pengenalanJavascript)
    Source code modul ke-lima, mengenai Javascript Dasar beserta latihannya

---
+++ tugas-modul-5 (pengenalanJavascript)/modul4.js
@@ -0,0 +1,11 @@
+hasil = confirm("melanjutkan : ");
+if (hasil == true) 
+{
+    hasil = prompt ("Masukan Nilai ");
+    if (hasil != "") {
+            alert("Anda memasukan nilai : " + hasil); 
+        }
+        else {
+            alert("Anda tidak memasukan nilai");
+        } 
+}
